eaa9f8c6ab0bd8ae9415dd1f7942b4ec22516650
ManuelExpunged <manuelstraussaigner@gmail.com>
2021-03-08 02:19:45 +0100

    Huffman class

--- Huffman/Huffman.cs
+++ Huffman/Huffman.cs
@@ -1,8 +1,4 @@
-﻿using Huffman.Models;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Huffman
@@ -16,92 +12,9 @@ namespace Huffman
 
         private void ConvertBtn_Click(object sender, EventArgs e)
         {
-            char[] chars = InputBox.Text.ToArray();
-
-            Dictionary<char, int> numOfChar = new Dictionary<char, int>();
-
-            foreach (char c in chars)
-            {
-                if (!numOfChar.ContainsKey(c))
-                {
-                    numOfChar.Add(c, 1);
-                }
-                else
-                {
-                    numOfChar[c]++;
-                }
-            }
-
-            List<NodeModel> baseNodes = numOfChar.OrderByDescending(c => c.Value).Select(c => new NodeModel { Key = c.Key, Value = c.Value, Connector = ConnectorEnum.Connector.Empty }).ToList();
-            List<NodeModel> huffman = CreateHuffmanTree(baseNodes);
-            List<string> binaries = new List<string>();
-
-            foreach (char c in InputBox.Text.ToArray())
-            {
-                foreach (NodeModel node in huffman)
-                {
-                    if (c == node.Key)
-                    {
-                        binaries.Add(EncodeString(string.Empty, node));
-                    }
-                }
-            }
-
-            BinaryBox.Text = string.Join(" ", binaries);
-            //TreeBox.Text = CreateHuffmanTreeRepresentation(string.Empty, huffman[0]);
-        }
-
-        private string CreateHuffmanTreeRepresentation(string tree, NodeModel node)
-        {
-            if (node.Parents != null)
-            {
-                tree += "*";
-            }
-            else
-            {
-                tree += node.Key;
-            }
-            
-            if (node.Parents != null)
-            {
-                foreach (NodeModel parentNode in node.Parents)
-                {
-                    tree = CreateHuffmanTreeRepresentation(tree, parentNode);
-                }
-            }
-
-            return tree;
-        }
-
-        private List<NodeModel> CreateHuffmanTree(List<NodeModel> nodes)
-        {
-            List<NodeModel> childlessNodes = nodes.Where(n => n.Child == null).ToList();
-
-            if (childlessNodes.Count > 1)
-            {
-                NodeModel min1 = childlessNodes[^1];
-                NodeModel min2 = childlessNodes[^2];
-
-                nodes.Add(new NodeModel { Parents = new NodeModel[] { min1, min2 }, Value = min1.Value + min2.Value, Connector = ConnectorEnum.Connector.Empty });
-
-                min1.Child = nodes[^1];
-                min1.Connector = ConnectorEnum.Connector.Left;
-                min2.Child = nodes[^1];
-                min2.Connector = ConnectorEnum.Connector.Right;
-
-                nodes = CreateHuffmanTree(nodes.OrderByDescending(n => n.Value).ToList());
-            }
-            return nodes;
-        }
-
-        private string EncodeString(string binary, NodeModel node)
-        {
-            if (node.Connector != ConnectorEnum.Connector.Empty)
-            {
-                binary += ((int)node.Connector).ToString();
-                binary = EncodeString(binary, node.Child);
-            }
-            return binary;
+            HuffmanTree huffmanTree = new HuffmanTree(InputBox.Text);
+            BinaryBox.Text = string.Join(" ", huffmanTree.Encode());
+            ////TreeBox.Text = CreateHuffmanTreeRepresentation(string.Empty, huffman[0]);
         }
     }
 }
---
+++ Huffman/HuffmanTree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Huffman
+{
+    public class HuffmanTree
+    {
+        private readonly char[] letters;
+        private readonly Dictionary<char, int> countedLetters = new Dictionary<char, int>();
+        private Node root;
+
+        public HuffmanTree(string src)
+        {
+            letters = src.ToCharArray();
+            CountLetters();
+            BuildTree();
+        }
+
+        private void BuildTree()
+        {
+            List<Node> nodes = countedLetters
+                .Select(c => new Node { Letter = c.Key, Count = c.Value })
+                .ToList();
+
+            while (nodes.Count > 1)
+            {
+                List<Node> sortedNodes = nodes.OrderBy(n => n.Count).ToList();
+
+                Node parent = new Node()
+                {
+                    Letter = null,
+                    Count = sortedNodes[0].Count + sortedNodes[1].Count,
+                    Left = sortedNodes[0],
+                    Right = sortedNodes[1]
+                };
+
+                nodes.Remove(sortedNodes[0]);
+                nodes.Remove(sortedNodes[1]);
+                nodes.Add(parent);
+            }
+
+            root = nodes.FirstOrDefault();
+        }
+
+        private void CountLetters()
+        {
+            foreach (char c in letters)
+            {
+                if (!countedLetters.ContainsKey(c))
+                {
+                    countedLetters.Add(c, 1);
+                }
+                else
+                {
+                    countedLetters[c]++;
+                }
+            }      
+        }
+
+        public List<string> Encode()
+        {
+            List<string> encodedSrc = new List<string>();
+            encodedSrc.AddRange(letters.Select(c => string.Join(string.Empty, root.Traverse(c, new List<bool>()).Select(b => b ? "1" : "0"))));
+            return encodedSrc;
+        }
+    }
+
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public char? Letter { get; set; }
+        public int Count { get; set; }
+
+        public List<bool> Traverse(char? c, List<bool> data)
+        {
+            if (Left == null && Right == null)
+            {
+                return (bool)c?.Equals(Letter) ? data : null;
+            }
+            else
+            {
+                List<bool> left = null;
+                List<bool> right = null;
+
+                if (Left != null)
+                {
+                    List<bool> leftPath = new List<bool>(data);
+                    leftPath.Add(false);
+                    left = Left.Traverse(c, leftPath);
+                }
+
+                if (Right != null)
+                {
+                    List<bool> rightPath = new List<bool>(data);
+                    rightPath.Add(true);
+                    right = Right.Traverse(c, rightPath);
+                }
+
+                return left ?? right;
+            }
+        }
+    }
+}
